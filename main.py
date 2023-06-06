63f8fb4485bea8159f9c16efb485f6280bfe35ac
Admin <emsexy6969@gmail.com>
2023-06-05 22:10:36 -0400

    added cors

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://nextjs-frontend.herokuapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get('/image')
 def get_image():
     image_url = 'https://i.imgur.com/TNzYa7Q.jpg'  # Replace with your own image URL
